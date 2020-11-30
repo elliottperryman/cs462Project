e2e51d0f41b497eae03b9d3dca1d0968a41a3b3b
David Perryman <dperrym2@lbl.gov>
2020-11-30 09:40:18 -0800

    mat. mult results

--- matrix_multiply.py
+++ matrix_multiply.py
@@ -7,23 +7,24 @@ def main():
     N = 2**(int(argv[1]))
 
     import dask.array as da
+    
+    from time import time
 
     from dask.distributed import Client
-    client = Client(scheduler_file='./scheduler.json')
+    client = Client(n_workers=int(argv[1]), threads_per_worker=2, dashboard_address=None, scheduler_port=0)
+    print(client)
 
-    x = da.random.random(size=(N,N))
-    y = da.random.random(size=(N,N))
-
-    prod = x.dot(y)
-
-    import numpy as np
-
-
-    np.save('tmp', prod.compute())
+    for exp in range(1,15):
+        N = 2**exp
+        t1 = time()
+        x = da.random.random(size=(N,N))
+        y = da.random.random(size=(N,N))
+        z = x.dot(y).compute() 
+        t2 = time()
+        print('exp:',exp,'\t N:',)
+        print('time: ',t2-t1)
 
 
 if __name__=='__main__':
     main()
-else:
-    print(__name__)
 
